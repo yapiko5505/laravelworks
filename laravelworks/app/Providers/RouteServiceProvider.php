d82df2fbeb1669118305dc05ebc3ec2eeefdff5a
yapiko5505 <yapiko5505@users.noreply.github.com>
2023-01-29 12:57:01 +0900

    laravelworks3

--- laravelworks/app/Providers/RouteServiceProvider.php
+++ laravelworks/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    // public const HOME = '/dashboard';
+
+    // 追加コード
+    public const HOME = '/post';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
